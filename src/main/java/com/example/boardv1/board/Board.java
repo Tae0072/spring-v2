f7cf6497a1ef13d479d2e786a59eef74bff4936a
Tae0072 <rkdxodh41@gmail.com>
2026-02-02 16:25:57 +0900

    회원가입완료

--- src/main/java/com/example/boardv1/board/Board.java
+++ src/main/java/com/example/boardv1/board/Board.java
@@ -26,6 +26,7 @@ public class Board {
     private Integer id;
     private String title;
     private String content;
+
     @CreationTimestamp
     private Timestamp createdAt;
 
---
+++ src/main/java/com/example/boardv1/user/UserRepository.java
@@ -0,0 +1,41 @@
+package com.example.boardv1.user;
+
+import java.lang.foreign.Linker.Option;
+
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Repository
+public class UserRepository {
+
+    // DI
+    private final EntityManager em;
+
+    // 회원가입할 때 insert
+    public User save(User user) {
+        em.persist(user);
+        return user;
+    }
+
+    // 로그인할때 username으로 조회해서 password 검증
+    public User findByUsername(String username) {
+        Query query = em.createQuery("select u from User u where u.username = :username", User.class);
+        query.setParameter("username", username);
+        try {
+            User findUser = (User) query.getSingleResult();
+            return findUser;
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    public void findById(int id) {
+        User user = em.find(User.class, id);
+
+    }
+}
